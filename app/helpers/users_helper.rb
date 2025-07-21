ee22139d537a5b3d9500a6fe29b6c03325f235cb
ngtrutuo1 <nguyen.trung.tuong@sun-asterisk.com>
2025-07-21 18:09:01 +0700

    chapter_7_sign_up

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,15 @@
+module UsersHelper
+  def gravatar_for user
+    gravatar_id = Digest::MD5.hexdigest user.email.downcase
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
+
+  def gender_options
+    [
+      [t("activerecord.attributes.user.female"), :female],
+      [t("activerecord.attributes.user.male"), :male],
+      [t("activerecord.attributes.user.other"), :other]
+    ]
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  USER_PERMIT = %i(name email password password_confirmation date_of_birth
+gender).freeze
+
   VALID_EMAIL_REGEX = Regexp.new(Settings.development.user.valid_email_regex,
                                  Regexp::IGNORECASE)
   PASSWORD_REQUIREMENT =
@@ -8,19 +11,19 @@ class User < ApplicationRecord
 
   validates :name, presence: true,
                   length: {maximum: Settings.development.user
-                                            .max_name_length.to_i,
-                           message: :too_short}
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false},
-                    length: {maximum: Settings.development.user
-                                              .max_email_length.to_i}
+                                            .max_name_length.to_i}
+  validate :full_name_must_contain_first_and_last
+  validates :email, presence: true,
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false},
+            length: {maximum: Settings.development.user
+                                      .max_email_length.to_i}
   validates :password, length: {minimum: Settings
     .development.user.min_password_length.to_i},
-            format: {with: PASSWORD_REQUIREMENT,
-                     message: :password_requirement_message},
-            if: ->{new_record? || !password_digest.nil?}
+            format: {with: PASSWORD_REQUIREMENT}
   validates :date_of_birth, presence: true
   validate :dob_validation
+  validates :gender, presence: true
 
   before_save :downcase_email
 
@@ -41,5 +44,13 @@ class User < ApplicationRecord
     elsif date_of_birth < prev_date
       errors.add(:date_of_birth, :over_hundred_years)
     end
+  end
+
+  def full_name_must_contain_first_and_last
+    return if name.blank?
+
+    return unless name.strip.split.size < 2
+
+    errors.add(:name, :must_contain_first_and_last)
   end
 end
